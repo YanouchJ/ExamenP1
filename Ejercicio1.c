06b6902b9d5118ff4e4bf9b6f095e4a244f6c5ca
Yanouch.J <jeimy.yanouch@udla.edu.ec>
2023-11-01 16:05:44 -0500

    Implementación de ciclo para validar la suma hasta 1200

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -4,9 +4,24 @@ int main(int argc, char const *argv[])
 {
     /* Realice un programa para hallar cuántos números se debieron haber leído (ingresados por el usuario) para que la suma de los números negativos dé en valor absoluto mayor que 1200. */
      
-     int num;
-     printf("Ingrese un numero: ");
-     scanf("%i", &num);
+    int num, ingresados, total, suma;
+    printf("Ingrese un numero: ");
+    scanf("%d", &num);
+    suma=num;
+    total=suma;
 
+    for ( total = 0; total > 1200; total++)
+    {
+        printf("Ingrese un numero: ");
+        scanf("%d", &num);
+    }
+    
+    /*while (total<=1200)
+    {
+        printf("Ingrese un numero: ");
+        scanf("%d", &num);
+    }*/
+
+    printf("Numeros ingresados necesarios: %d, ", num);
     return 0;
 }
