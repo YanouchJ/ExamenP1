a717181404640aeeb1c64d374b90596511a3b426
Yanouch.J <jeimy.yanouch@udla.edu.ec>
2023-11-01 15:35:21 -0500

    Definición del ejercicio y declaración de primeras variables

---
+++ Ejercicio1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* Realice un programa para hallar cuántos números se debieron haber leído (ingresados por el usuario) para que la suma de los números negativos dé en valor absoluto mayor que 1200. */
+     
+     int num;
+     printf("Ingrese un numero: ");
+     scanf("%i", &num);
+
+    return 0;
+}
